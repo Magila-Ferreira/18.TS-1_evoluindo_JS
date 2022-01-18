90d4134e5c032944bd3bae3098434b420bedb5b5
Mágila-Ferreira <magilamorganasf@hotmail.com>
2022-01-17 22:01:48 -0300

    17/01/22_Aula 3 - Benefícios da tipagem estática

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,25 @@
-import { Negociacao } from "./models/negociacao.js";
+// AULA 1
 
+import { Negociacao } from "./models/negociacao.js";
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+/* INSTANCIANDO A CLASSE_NEGOCIAÇÃO */
 const negociacao = new Negociacao(new Date(), 10, 100);
 console.log(negociacao.volume);
+
+/* INSTANCIANDO A CLASSE_NEGOCIAÇÃO-CONTROLLER */
+const controller = new NegociacaoController();
+
+/* LINKE DO FORMULÁRIO AO JS */
+const form = document.querySelector('.form');
+
+/* ADICIONA UM EVENTO DO TIPO SUBMIT AO FORMULÁRIO */
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    /* O EVENTO CHAMA A FUNÇÃO_ADICIONA DA CLASSE_NEGOCIAÇÃO-CONTROLLER */
+    controller.adiciona();
+})
+
 
 
 /* RODAR APLICAÇÃO NO NAVEGADOR:
